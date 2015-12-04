1477a250c41d2089a98bc325aaf1345a8d236e69
Fergus Lemon <fergus.lemon@gmail.com>
2015-12-04 17:12:33 +0000

    update

--- my_static_file_server.js
+++ my_static_file_server.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
-var port = process.env.PORT || 8080;
+app.use(express.static(__dirname));
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.listen(port, function() {
-  console.log("This app is running on http//localhost:" + port);
-});
+app.listen(8080);
--- public/js/token.js
+++
@@ -1,1 +0,0 @@
-var token = "5ff50eac4cdc5f12795fc7cb205fc4e7caaa99a8";
