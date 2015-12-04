2dabb55d401b8fe9809afda3054ef8a3d2916f45
Fergus Lemon <fergus.lemon@gmail.com>
2015-12-04 13:25:55 +0000

    test heroku

--- my_static_file_server.js
+++ my_static_file_server.js
@@ -1,14 +1,10 @@
 var express = require('express'),
     app = express();
 
-// var port = process.env.PORT || 8080;
-
-// app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/js'));
 
-// app.get('/', function(request, response) {
-//   response.render('index');
-// });
+app.get('/', function(request, response) {
+  response.render('index');
+});
 
 app.listen(process.env.PORT || 8080);
