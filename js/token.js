a5f24c1a036023379f6e64a82dedcb1cb9f747ca
Fergus Lemon <fergus.lemon@gmail.com>
2015-12-04 13:14:13 +0000

    test heroku

---
+++ js/token.js
@@ -0,0 +1,1 @@
+var token = "293b68f1ec964ff0df944d5d2bd7767e7ca12433";
--- my_static_file_server.js
+++ my_static_file_server.js
@@ -1,16 +1,14 @@
 var express = require('express'),
     app = express();
 
-var port = process.env.PORT || 8080;
+// var port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('index.html');
 });
 
-app.listen(port, function() {
-  console.log('This app is running on http://localhost:' + port);
-});
+app.listen(8080);
